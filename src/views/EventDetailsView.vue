82f7fc8a75043aaf4258c49d7d7e0cd318a139e7
Nurlan <hotabyc8@gmail.com>
2024-01-20 20:36:59 +0400

    First commit

---
+++ src/views/EventDetailsView.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import {onMounted, ref} from "vue";
+import EventService from "../../services/EventService.js";
+
+const event = ref(null);
+const props = defineProps({
+  id: {
+    required: true
+  }
+})
+
+onMounted(() => {
+  EventService.getEvent(props.id)
+    .then((res) => event.value = res.data)
+})
+</script>
+
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} at {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
